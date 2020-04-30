896c1040ce707c2f06164e7750c1442393b0c427
Jorge Hecherat <hecherat@gmail.com>
2020-04-29 21:30:17 -0300

    Debugger config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({
-    message: 'Hello GoStack',
-  }),
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  response.json(user);
+});
 
 export default routes;
