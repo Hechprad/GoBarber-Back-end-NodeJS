a12766f8eb6d6a3b98af4a3119561121f5349c26
Jorge Hecherat <hecherat@gmail.com>
2020-04-30 23:19:20 -0300

    [develop] - Appointments list route added

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,6 +6,10 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  response.json(appointmentsRepository.all());
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
   const parsedDate = startOfHour(parseISO(date));
@@ -19,9 +23,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
-
-  return response.json(appointment);
+  return response.json(appointmentsRepository.create(provider, parsedDate));
 });
 
 export default appointmentsRouter;
