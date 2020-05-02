b32f0ec1cdd2292336347f789bd958dff900ff26
Jorge Hecherat <hecherat@gmail.com>
2020-05-02 18:41:51 -0300

    [develop] - async added to service and default value added to appointment migration

--- src/database/migrations/1588442383949-CreateAppointments.ts
+++ src/database/migrations/1588442383949-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1588442383949
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
