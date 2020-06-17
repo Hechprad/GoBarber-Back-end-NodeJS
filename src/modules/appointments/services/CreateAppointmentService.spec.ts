b8ad8f3bdc84276db9232a147c78a1468a6563cb
Jorge Hecherat <hecherat@gmail.com>
2020-06-16 21:19:21 -0300

    [NV04-arquitetura-e-testes-no-NodeJS] - First Unit test added

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,26 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+// Unit tests
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect().toBe();
+  // });
+});
