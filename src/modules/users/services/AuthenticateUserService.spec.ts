5552e948b6bb114d7d92e9b09302edade29245af
Jorge Hecherat <hecherat@gmail.com>
2020-06-18 20:05:20 -0300

    [NV04-arquitetura-e-testes-no-NodeJS] - Authentication tests

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,37 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
+
+// Unit tests
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'Jorge Hecherat',
+      email: 'test@test.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'test@test.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});
