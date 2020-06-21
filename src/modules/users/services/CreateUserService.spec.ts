43e7c05376dc7c13df196295d901faa97cd98c91
Jorge Hecherat <hecherat@gmail.com>
2020-06-21 16:34:11 -0300

    [NV04-continuando-back-end-do-app] - Async tests fix

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -41,7 +41,7 @@ describe('CreateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       createUser.execute({
         name: 'Jorge Hecherat',
         email: 'test@test.com',
