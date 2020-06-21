21f61110743021051aa37fd27424d869d530caa8
Jorge Hecherat <hecherat@gmail.com>
2020-06-21 17:32:05 -0300

    [NV04-continuando-back-end-do-app] - User forgot password

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,15 @@ import { container } from 'tsyringe';
 
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+// import DiskStorageProvider from '@shared/container/providers/MailProvider/implementations/;
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
+
+// container.registerSingleton<IMailProvider>(
+//   'MailPRovider',
+//   ??,
+// );
