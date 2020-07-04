c69528e503cb1b0a5993f7c946d54f4f926f5317
Jorge Hecherat <hecherat@gmail.com>
2020-07-04 17:07:03 -0300

    [NV04-continuando-back-end-do-app] - new availability routes added

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,26 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '@modules/appointments/infra/http/controlers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controlers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controlers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
