28a7b623139b1d51f457ab31b6a3dcb05cfe4c58
Jorge Hecherat <hecherat@gmail.com>
2020-07-05 22:13:46 -0300

    [NV04-finalizando-back-end-do-app] - List provider appointments

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,39 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+// Unit tests
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      user_id: 'user',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+    const appointment2 = await fakeAppointmentsRepository.create({
+      user_id: 'user',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 20,
+      year: 2020,
+      month: 5,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
